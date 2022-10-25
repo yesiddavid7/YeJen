6e2eb8c7b15c0432e771257dc8eab04bf4ee893e
Yesid David <yesid.david7@gmail.com>
2022-10-25 13:38:53 -0500

    Octavo Commit

--- front/src/App.js
+++ front/src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import React from 'react';
 import Header from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
+import Home from './components/Home';
+//Router traido desde react-router-dom
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <div className='container container-fluid'>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Home" element={<Home/>}/>
+          </Routes>
+        </div>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <nav className='navbar row'>
             <div className='col-12 col-md-3'>
                 <div className='navbar-brand'>
-                    <img src="./images/logo.png' alt='YeJen Logo"></img>
+                    <img className='image-logo' src="./images/logo.png" alt="YeJen Logo"></img>
                 </div>
             </div>
             <div className='col-12 col-md-6 mt-2 mt-md-0'>
@@ -18,15 +18,16 @@ const Header = () => {
                         placeholder='Que producto Busca?'/>
                     <div className='input-group-append'>
                         <button id="search-btn" class="btn">
-                            <i class= "fa fa-search-plus fa-2x" aria-hidden="true"/>
+                            <i className= "fa fa-search-plus fa-lg text-white" aria-hidden="true"/>
                         </button>
                     </div>
                 </div>
             </div>
-            <div className='col-12 col-md-3 mt-4 mt-md-0 text-center'>
-                <span><button className='btn' id="login-btn">Inicie Sesion</button></span>
-                <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
-                <span className='ml-1' id='cart-count'>2</span>
+            <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                <span><button type="button" class="btn btn-outline-warning">Inicie Sesion</button></span>
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <i className="fa fa-shopping-cart fa-lg text-white" aria-hidden="true"/>
+                <span className="ml-1" id="cart_count">2</span>
             </div>
         </nav>
     </Fragment>
